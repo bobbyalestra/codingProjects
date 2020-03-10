063bc194e24242a0dcf78ed621083d9461c35f33
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-10 08:31:54 -0400

    first loop program

---
+++ javascript/101/forloops.js
@@ -0,0 +1,13 @@
+
+//  set up for ([initialization]; [condition]; [final-expression])
+
+var myArray = [];
+
+for (var i = 0; i < 5; i++){
+    myArray.push(i);
+  
+  }
+  
+  console.log(myArray)
+
+  //will display [0,1,2,3,4]. Changing the variable i so it = 1 and making i < 6, you will get a log of [1,2,3,4,5]
