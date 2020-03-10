87a1c12fe2d3187d48683052689c0cf1c8cb71e2
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-10 10:33:25 -0400

    decrease order array

--- javascript/101/forloops.js
+++ javascript/101/forloops.js
@@ -22,3 +22,28 @@ for (var i = 1; i < 11; i += 2) {
 }
 
 console.log(myArray)
+
+
+//next example is starting at a number in decreasing it buy two.
+
+var myArray = [];
+
+for (var  i = 10; i > 0; i-=2){
+  myArray.push(i)
+}
+
+console.log(myArray)
+
+// this displays [10,8,6,4,2,0]. 
+//by changing var i to equal 9 , it will start off at 9 and decrease by two until you hit number one
+var myArray = [];
+
+for (var  i = 9; i > 0; i-=2){
+  myArray.push(i)
+}
+
+console.log(myArray)
+
+//next we will be console logging all of var i as an array
+
+var myArr = [ 2, 3, 4, 5, 6];
