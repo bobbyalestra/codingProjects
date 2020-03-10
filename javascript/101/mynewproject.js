c6ddc671484253a0b4746467dc71e375a646f383
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-09 20:52:05 -0400

    second project

---
+++ javascript/101/mynewproject.js
@@ -0,0 +1,12 @@
+let businessName = "Felicia's lemonade Stand";
+let slogan = "Bye Felicia";
+let fruit1 = "Apples";
+let fruit2 = "Oranges";
+let fruit1cost = .97;
+let fruit2cost = 1;
+let fruit1sales = 60;
+let fruit2sales = 25;
+
+console.log(businessName);
+console.log(`${fruit1} cost ${fruit1cost} each. Today we sold ${fruit1sales} ${fruit1}. For a  total of ${fruit1cost * fruit1sales} in sales.`)
+console.log(`${fruit2} cost ${fruit2cost} each. Today we sold ${fruit2sales} ${fruit2}. For a  total of ${fruit2cost * fruit2sales} in sales.`)
