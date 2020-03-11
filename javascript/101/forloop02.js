434635b6e91b3966dcdbe56fb0d05d1b1a2262d1
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-10 21:48:43 -0400

    for loop for class project console log 4 places you want to go

---
+++ javascript/101/forloop02.js
@@ -0,0 +1,6 @@
+let places= ["Somewhere Warm", "Somewhere Cold", "Italy", "My next door neighbors house"]
+ 
+for (let i = 0; i < 1; i++){
+console.log(places) //will display all variables are in array
+
+}
