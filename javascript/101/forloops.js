16b7cf7fd97956236b71c93e9e853d68ddf7d78c
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-13 13:38:02 -0400

    iterate through all an arrays items using for loops

--- javascript/101/forloops.js
+++ javascript/101/forloops.js
@@ -82,3 +82,20 @@ for (let i = 0; i < cohort11e.length; i++){
 console.log(cohort11e.length) //will display how man variables are in array
  // console.log(cohort11e[2]) // will select secind name
 } 
+
+
+
+// Iterate Through All an Array's Items Using For Loops
+
+function filteredArray(arr, elem) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem)==-1) { // add.index(elem) == -1 as the condition
+      newArr.push(arr[i]);
+    }
+  }
+    return newArr; 
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
