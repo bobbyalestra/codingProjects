98a6b70d407b64eb775f43d5f4a6f54ec15c0415
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-18 14:54:50 -0400

    updates

--- javascript/101/findingNth.js
+++ javascript/101/findingNth.js
@@ -17,3 +17,4 @@ console.log(`${firstLetter} ${lastInitial}`) ;
 (initials());
 
 
+''
---
+++ javascript/101/passingfunctions.js
@@ -0,0 +1,8 @@
+
+function locations(originCity, destinationCity) {
+
+ if (originCity = "Falmouth"){
+    console.log(`I was born in one of the most boring towns on the Cape, ${originCity} and I still live in ${destinationCity}`)
+ }
+}
+locations('Falmouth', 'Falmouth')
