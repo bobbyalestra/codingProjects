5f95f6540ecdeb3227de1d5d30efd5d7f70419f8
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-19 17:34:39 -0400

    updates

--- javascript/101/findingNth.js
+++ javascript/101/findingNth.js
@@ -7,7 +7,10 @@ let lastInitial = lastName[0];
 console.log(`${firstLetter} ${lastInitial}`) ;
  }
 (firstNameLastName());
- function initials(){
+ 
+//more practice trying it again with my sons name
+
+function initials(){
     let firstName = "Cameron"
     let firstLetter = firstName[0];
     let lastName = "Alestra";``
@@ -16,5 +19,11 @@ console.log(`${firstLetter} ${lastInitial}`) ;
 }
 (initials());
 
+//simplifying and just trying to find numbers
 
-''
+function numbers (){
+numbers = [3,14,53,67,76,77,9]
+
+console.log(numbers[3])
+}
+(numbers());
