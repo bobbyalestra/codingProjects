c78b02d83db66e85214611d2783b52e71f43c5d9
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-24 18:24:10 -0400

    updates

--- javascript/101/everythingsofar.js
+++ javascript/101/everythingsofar.js
@@ -2,17 +2,21 @@
 // first array logs 7 ,4 ,7 five undefined?
 
 let typeOfFriends = ["Childhood", "Work", "School"];
-let myFriends = [["Matty J", "Andy", "Erika", "Stevie"], ["Drew", "Michelle", "Maddie", "Tommy"], ["Arneezy", "Chelsea", "Will I Am", "Margie"]];
+let myFriends = [["Matty J", "Andy", "Erika" ], ["Drew", "Michelle", "Maddie"], ["Arneezy", "Chelsea", "Will I Am"]];
+
 
 for (i = 0; i < typeOfFriends.length; i++){
-    for (j = 0; j < typeOfFriends[i].length; j++)
-
+    for (j = 0; j < myFriends[i].length; j++)
 console.log( `${typeOfFriends[i]}: ${myFriends[i][j]} `)
 }
 
-// i cant get all four elements
-for (i = 0; i < typeOfFriends.length; i++){
-    for (j = 0; j < myFriends.length; j++)
 
-console.log( `${typeOfFriends[i]}: ${myFriends[i][j]} `)
+let townNames = ["Falmouth", "Narragansette", "Bourne"];
+let beachViews = [["Old Silver", "The Heights", "Megansette" ], ["Narragasette", "Another part of Narragansette", "More Narragansette"], ["ScraggY Neck", "The Harbor", "Train Bridge"]];
+
+
+for (i = 0; i < townNames.length; i++){
+    for (j = 0; j < beachViews[i].length; j++)
+ console.log( `${townNames[i]}: ${beachViews[i][j]}: `)
 }
+
---
+++ javascript/101/moreloopinloop.js
@@ -0,0 +1,15 @@
+let townNames = ["Falmouth", "Narragansette", "Bourne"];
+let beachViews = [["Old Silver", "The Heights", "Megansette" ], ["Narragasette", "Another part of Narragansette", "More Narragansette"], ["ScraggY Neck", "The Harbor", "Train Bridge"]];
+let numOfYears = [[30, 2, 1],[1, 2, 3], [ 1, 4, 2]];
+
+for (i = 0; i < townNames.length; i++){
+    for (j = 0; j < townNames[i].length; j++){
+        for (k = 0; k < townNames[i].length; k++)
+    
+
+
+
+console.log( `${townNames[i]}: ${beachViews[i][j]}: ${numOfYears[i][j][k]} `)
+}
+
+}
