6ba1544c914064f4d6aa194562de0366e578be0b
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-25 10:51:47 -0400

    updates and fixes

--- javascript/101/everythingsofar.js
+++ javascript/101/everythingsofar.js
@@ -3,20 +3,12 @@
 
 let typeOfFriends = ["Childhood", "Work", "School"];
 let myFriends = [["Matty J", "Andy", "Erika" ], ["Drew", "Michelle", "Maddie"], ["Arneezy", "Chelsea", "Will I Am"]];
-
+let numOfYears = [[30,28, 26],  [2,1,3], [1,1,1]]
 
 for (i = 0; i < typeOfFriends.length; i++){
     for (j = 0; j < myFriends[i].length; j++)
-console.log( `${typeOfFriends[i]}: ${myFriends[i][j]} `)
+console.log( `${typeOfFriends[i]}: ${myFriends[i][j]} ${numOfYears[i][j]} `)
 }
 
 
-let townNames = ["Falmouth", "Narragansette", "Bourne"];
-let beachViews = [["Old Silver", "The Heights", "Megansette" ], ["Narragasette", "Another part of Narragansette", "More Narragansette"], ["ScraggY Neck", "The Harbor", "Train Bridge"]];
-
-
-for (i = 0; i < townNames.length; i++){
-    for (j = 0; j < beachViews[i].length; j++)
- console.log( `${townNames[i]}: ${beachViews[i][j]}: `)
-}
 
--- javascript/101/moreloopinloop.js
+++ javascript/101/moreloopinloop.js
@@ -5,11 +5,18 @@ let capacityNum = [[150, 200, 75],[100, 200, 300], [ 175, 400, 250]];
 for (i = 0; i < townNames.length; i++){
     for (j = 0; j < beachViews[i].length; j++){
        
-    
-
-
-
-console.log( `The beach, ${beachViews[i][j]}: located in ${townNames[i]}: can hold a maximum of  ${capacityNum[i][j]} people `)
+    console.log( `The beach, ${beachViews[i][j]}: located in ${townNames[i]}: can hold a maximum of  ${capacityNum[i][j]} people `)
 }
 
 }
+ // second example
+
+let typeOfFriends = ["Childhood", "Work", "School"];
+let myFriends = [["Matty J", "Andy", "Erika" ], ["Drew", "Michelle", "Maddie"], ["Arneezy", "Chelsea", "Will I Am"]];
+let numOfYears = [[30,28, 26],  [2,1,3], [1,1,1]]
+
+for (i = 0; i < typeOfFriends.length; i++){
+    for (j = 0; j < myFriends[i].length; j++)
+console.log( `${typeOfFriends[i]}: ${myFriends[i][j]} ${numOfYears[i][j]} `)
+}
+
