df011919ec8624857f033b802d1d22700f128d36
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-25 10:48:40 -0400

    more projects

--- javascript/101/moreloopinloop.js
+++ javascript/101/moreloopinloop.js
@@ -1,15 +1,15 @@
 let townNames = ["Falmouth", "Narragansette", "Bourne"];
 let beachViews = [["Old Silver", "The Heights", "Megansette" ], ["Narragasette", "Another part of Narragansette", "More Narragansette"], ["ScraggY Neck", "The Harbor", "Train Bridge"]];
-let numOfYears = [[30, 2, 1],[1, 2, 3], [ 1, 4, 2]];
+let capacityNum = [[150, 200, 75],[100, 200, 300], [ 175, 400, 250]];
 
 for (i = 0; i < townNames.length; i++){
-    for (j = 0; j < townNames[i].length; j++){
-        for (k = 0; k < townNames[i].length; k++)
+    for (j = 0; j < beachViews[i].length; j++){
+       
     
 
 
 
-console.log( `${townNames[i]}: ${beachViews[i][j]}: ${numOfYears[i][j][k]} `)
+console.log( `The beach, ${beachViews[i][j]}: located in ${townNames[i]}: can hold a maximum of  ${capacityNum[i][j]} people `)
 }
 
 }
