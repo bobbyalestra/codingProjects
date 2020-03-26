457982bbbf26f18bdb33a98983f2434a7696aaae
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-26 12:27:23 -0400

    updates
    updates

---
+++ javascript/101/fruityloops.js
@@ -0,0 +1,17 @@
+
+
+let myArray = []
+let i = 1;
+let n = 100
+
+
+do {
+    i = i + 2;
+    console.log(i)
+}while(i < 99)
+ 
+
+for (n = 100; n > -2; n -= 2){
+    myArray.push(n);
+ }
+ console.log(myArray)
---
+++ javascript/101/newremainder.js
@@ -0,0 +1,5 @@
+let x = 5;
+let y = 2;
+
+console.log(x % y)
+ 
