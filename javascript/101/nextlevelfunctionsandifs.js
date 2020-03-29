336b40b22d2a4190b6825065b667904d44270512
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-29 19:17:43 -0400

    updates

---
+++ javascript/101/nextlevelfunctionsandifs.js
@@ -0,0 +1,25 @@
+let x = 7;
+let y = 5;
+
+
+function firstNum(x)
+  function secondNum(y)
+
+while (x = 7, y= 5){
+ 
+  
+  if (x < y){
+    return true
+  }
+  } 
+
+  console.log("true")
+  
+  firstNum(5);
+ secondNum(7);
+  
+
+
+
+
+
