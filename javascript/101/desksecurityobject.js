e461228af226a735c2f5dd78236f297920879d50
bobbyalestra <bobby.alestra135@gmail.com>
2020-03-31 21:07:35 -0400

    updates

--- javascript/101/desksecurityobject.js
+++ javascript/101/desksecurityobject.js
@@ -1,58 +1,61 @@
-let securityScanner = {
+let = securityScanner = {
+  currentBadgeIdScanned: "", // key: Value (property / kv pair)
+  currentEmployeeNameScanned: "",
+  badgeIdInBuilding: [],
+  employeeNameInBuilding: [],
+  badgeIdOutBuilding: [],
+  employeeNameOutBuilding: [],
+  inScan: function() {
+    this.badgeIdInBuilding.push(this.currentBadgeIdScanned);
+    this.employeeNameInBuilding.push(this.currentEmployeeNameScanned);
+    console.log(`:::::In The Building::::`);
+    console.log(`ID's: ${this.badgeIdInBuilding}`);
+    console.log(`Name's: ${this.employeeNameInBuilding}`);
+  },
+  outScan: function() {
+    if (this.badgeIdInBuilding == this.currentBadgeIdScanned) {
+      this.badgeIdOutBuilding.push(this.currentBadgeIdScanned);
+      this.employeeNameOutBuilding.push(this.currentEmployeeNameScanned);
+      console.log(`::::Out The Building::::`);
+      console.log(`ID's: ${this.badgeIdOutBuilding}`);
+      console.log(`Name's: ${this.employeeNameOutBuilding}`);
+    } else if (this.currentBadgeIdScanned !== this.badgeIdOutBuilding)
+      console.log(
+        `!!!WARNING  Badge Number: ${this.currentBadgeIdScanned}  Employee: ${this.currentEmployeeNameScanned} DID NOT ENTER THE BUILDING!!!`
+      );
+  }
+};
 
-    currentBadgeIdScanned: "",
-    currentEmployeeNameScanned : "",
-    badgeIdsInBuilding : [],
-    employeeNamesInBuilding : [],
-    badgeIdsOutBuilding : [],
-    employeeNamesOutBuilding : [],
-
-inScan: function(){ //in scan equals a method
-      this.badgeIdsInBuilding.push(securityScanner.currentBadgeIdScanned);
-
-    this.employeeNamesInBuilding.push(securityScanner.currentEmployeeNameScanned)},
-outScan: function(){ 
-        this.badgeIdsOutBuilding.push(securityScanner.currentBadgeIdScanned);
+// end of object
+function scanId(id, name, inside) {
+  securityScanner.currentBadgeIdScanned = id;
+  securityScanner.currentEmployeeNameScanned = name;
   
-      this.employeeNamesOutBuilding.push(securityScanner.employeeNamesOutBuilding);
-
-
-    console.log(`..:::::In The Building:::::..`);
-    console.log(` ID's: ${this.badgeIdsInBuilding}`);
-    console.log(` Name's: ${this.currentEmployeeNameScanned}`);
-    
-    
-    console.log(`..:::::Out of The Building:::::..`);
-    console.log(` ID's: ${this.badgeIdsOutBuilding}`);
-    console.log(` Name's: ${this.currentEmployeeNameScanned}`);
-
+  if (inside == "inside") {
+    securityScanner.inScan();
+    }
 }
+function scanId(id, name, out) {
+  securityScanner.currentBadgeIdScanned = id;
+  securityScanner.currentEmployeeNameScanned = name;
 
-}
-
-function scanId(id, name ,inout){
-
-
-securityScanner.currentBadgeIdScanned = id;
-securityScanner.currentEmployeeNameScanned = name;
-
-
-if (inout == "in"){
-   securityScanner.inScan();
-} else (inout == "out")
-    securityScanner.outScan()
+  if (out == "out") {
+    securityScanner.outScan();
+    }
 }
 
 
-
-scanId("12345", "Arneezy", "in");
-scanId("56692", "Jamie", "in");
-scanId("78447", "Bobby", "in");
-scanId("47894", "Margie", "in");
-
-
-// out of the building
-scanId("14514", "Chelsea", "out");
+scanId("12345", "Arneezy", "inside");
+//scanId("12345", "Arneezy", "out");
+scanId("56692", "Jamie", "inide");
+//scanId("56692", "Jamie", "out");
+scanId("78447", "Bobby", "inside");
+//scanId("78447", "Bobby", "out");
+scanId("47894", "Margie", "inside");
+//scanId("47894", "Margie", "out");
+scanId("14514", "Chelsea", "inside");
+//scanId("14514", "Chelsea", "out");
+//scanId("69856", "Joe", "inside");
 scanId("69856", "Joe", "out");
-
-
+//scanId("87234", "Lord 9th", "inside");
+scanId("87234", "Lord 9th", "out");
--- javascript/101/practice.js
+++ javascript/101/practice.js
@@ -1,10 +1,60 @@
-let num1 = function(x){
-let num2 = function(y)
+let securityScanner = {
 
-{do {
-    x = x +2;
-  console.log([x])
-  } while (x < 40){
+  currentBadgeIdScanned: "",
+  currentEmployeeNameScanned : "",
+  badgeIdsInBuilding : [],
+  employeeNamesInBuilding : [],
+  badgeIdsOutBuilding : [],
+  employeeNamesOutBuilding : [],
+ 
+inScan: function(){ //in scan equals a method
+    this.badgeIdsInBuilding.push(securityScanner.currentBadgeIdScanned);
+    this.employeeNamesInBuilding.push(securityScanner.currentEmployeeNameScanned)
+
+  console.log(`..:::::In The Building:::::..`);
+  console.log(` ID's: ${this.badgeIdsInBuilding}`);
+  console.log(` Name's: ${this.currentEmployeeNameScanned}`)
   
-  return num1(1)
-}}}
+},
+  outScan: function(){ 
+      this.badgeIdsOutBuilding.push(securityScanner.currentBadgeIdScanned);
+      this.employeeNamesOutBuilding.push(securityScanner.currentEmployeeNameScanned);
+
+  console.log(`..:::::Out of The Building:::::..`);
+  console.log(` ID's: ${this.badgeIdsOutBuilding}`);
+  console.log(` Name's: ${this.employeeNamesOutBuilding}`);
+
+function scanId(inout, id, name){
+
+securityScanner.currentBadgeIdScanned = id;
+securityScanner.currentEmployeeNameScanned = name;
+securityScanner.inside = inout;
+securityScanner.outside = inout;
+
+
+if (inout == "in"){
+  securityScanner.inScan();
+}   else (inout == "out")
+    securityScanner.outScan()
+}
+
+
+
+scanId("12345", "Arneezy", "in");
+scanId("12345", "Arneezy", "out");
+scanId("56692", "Jamie", "in");
+scanId("56692", "Jamie", "out");
+scanId("78447", "Bobby", "in");
+scanId("78447", "Bobby", "out");
+scanId("47894", "Margie", "in");
+scanId("47894", "Margie", "out");
+scanId("14514", "Chelsea", "in");
+scanId("14514", "Chelsea", "out");
+scanId("69856", "Joe", "in");
+scanId("69856", "Joe", "out");
+scanId("87234", "Lord 9th", "in");
+scanId("87234", "Lord 9th", "out");
+
+
+  }
+}
