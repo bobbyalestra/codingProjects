8e478f502741674be0d7431efd276f120e79fe8f
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-06 08:24:59 -0400

    updates

--- javascript/101/petshop.js
+++ javascript/101/petshop.js
@@ -18,15 +18,10 @@ function petShop (){
 let typeOfPet = [ 'Dog', 'Cat', "Lizard", 'Turtle'];
 let costOfPet = [ 10, 30, 40 , 55]
 
-fc
     return petShop[i]
 console.log(petShop);
 
 }
-
-
-
-
 
 function petShop(typeOfPets, costOfPets){
     typeOfPets ('Dog', 'Cat', 'Fish')
@@ -37,17 +32,13 @@ function petShop(typeOfPets, costOfPets){
       for (let i = 0 ; i < costOfPets.length; i ++)
        console.log( typeOfPets([i]), costOfPets([i]))
 }
-
-
-
-
 function typeOfPets(pet1, pet2, pet3, pet4) {
 function costOfPet(cost1, cost2, cost3, cost4)
  {
     console.log(pet1,pet2,pet3,pet4)
     console.log(cost1, cost2, cost3, cost4)
  
-
 typeOfPets('Dog', 'Cat', 'Fish', 'Turtle')
 costOfPet (5,6,88,56,35)
+}
 }
