b937f0c940431f75ecb071a2fdae2575d8496baa
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-11 11:13:00 -0400

    cams birthday code

--- 2020-04/fcc/convertto.js
+++
@@ -1,9 +0,0 @@
-function convertToF(celsius) {
-    let fahrenheit = celsius * 9/5 + 32;
-    return fahrenheit;
-  }
-  
-  convertToF(30);
-  
-
-  //The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
---
+++ cameronscode.js
@@ -0,0 +1,3 @@
+let name = "Cameron"
+
+console.log(` Happy Birthday ${name} !!!!! `)
