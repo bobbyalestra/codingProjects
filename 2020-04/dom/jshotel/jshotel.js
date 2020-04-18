7c395cf5821b9377df1a6ef8693998dfcede9682
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-17 20:36:20 -0400

    seperated my JS Hotel from other files

---
+++ 2020-04/dom/jshotel/jshotel.js
@@ -0,0 +1,46 @@
+
+let object= {
+  slogan : "I AM THE HACKER"
+}
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(events) {
+    if (!events.target.matches('.dropbtn')) {
+      var dropdowns1 = document.getElementsByClassName("dropdown1-content");
+      var i;
+      for (i = 0; i < dropdowns1.length; i++) {
+        var openDropdown1 = dropdowns[i];
+        if (openDropdown1.classList.contains('show')) {
+          openDropdown1.classList.remove('show');
+        }
+      }
+    }
+  }
