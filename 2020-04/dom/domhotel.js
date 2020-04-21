192841f7e8f9a45409c99056750c44d9ad42eaf8
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-21 00:15:46 -0400

    class project

--- 2020-04/dom/domhotel.js
+++ 2020-04/dom/domhotel.js
@@ -1,2 +1,7 @@
 document.getElementById('slogan').innerText = 'Vitory';
 document.getElementById('hotelName').innerText = 'The DOM Hotel'
+
+let availRooms = [101, 102, 103, 104]
+
+let linesOfHtml = "<select>"  
+linesOfHtml = linesOfHtml + `option value = '${availRooms[0]}'>${availRooms[0]}</option>
