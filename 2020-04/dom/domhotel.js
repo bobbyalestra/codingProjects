50aef8164f1836bb3bbbb03d4861ba50a9ddbb05
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-21 07:55:13 -0400

    adding last nights homework

--- 2020-04/dom/domhotel.js
+++ 2020-04/dom/domhotel.js
@@ -4,4 +4,5 @@ document.getElementById('hotelName').innerText = 'The DOM Hotel'
 let availRooms = [101, 102, 103, 104]
 
 let linesOfHtml = "<select>"  
-linesOfHtml = linesOfHtml + `option value = '${availRooms[0]}'>${availRooms[0]}</option>
+linesOfHtml = linesOfHtml + `<option value = '${availRooms[0]}'>${availRooms[0]} </option>`
+linesOfHtml = linesOfHtml + "</select>"
