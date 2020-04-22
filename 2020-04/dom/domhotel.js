f3c4fcce9075dd755ccab493dce4e3ddf6cbf1a3
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-22 17:03:43 -0400

    car rental app

--- 2020-04/dom/domhotel.js
+++ 2020-04/dom/domhotel.js
@@ -9,8 +9,7 @@ document.getElementById('availRoomslists"').innerHTML = linesOfHtml;
 let availRoomslist = [101, 102, 103, 104]
 
 let linesOfHtml = "<select>"  
-linesOfHtml = linesOfHtml + `<option value = '${availRoomslist[0]}'>${availRoomslist[0]} </option>`
-linesOfHtml = linesOfHtml + `<option value = '${availRoomslist[1]}'> ${availRoomslist[1]} </option>`
-linesOfHtml = linesOfHtml + `<option value = '${availRoomslist[2]}'>${availRoomslist[2]} </option>`
-linesOfHtml = linesOfHtml + `<option value = '${availRoomslist[3]}'>${availRoomslist[3]} </option>`
+for (i = 0; i < availRoomslist.lenth; i++)
+linesOfHtml = linesOfHtml + `<option value = '${availRoomslist[i]}'>${availRoomslist[i]} </option>`
+
 linesOfHtml = linesOfHtml + "</select>"
