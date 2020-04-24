3d7035e69876aeecfe80cb2a8ad454c82e659e1d
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-24 19:22:33 -0400

    started Arons Diabetes App

---
+++ sideprojects/aronDiabetesprogram.js
@@ -0,0 +1,82 @@
+console.log(`..:::MOMMY AND DADDY LOVE YOU SO MUCH ARON!!:::..`),
+
+let = itemScanner = {
+  todaysTime: "", // refers to the  parameters of the scanId "id"function
+  todaysDay: "", // refers to the  parameters of the scanId "name"function
+  currentBG: "", // refers to the  parameters of the scanId "price"function
+  targetBG: "", //  // refers to the  parameters of the scanId "amount"function
+  todaysTimesingroup: [],
+  todaysDayingroup: [],
+  lowBGlevel: [],
+  highBGlevel: [],
+  allitemsScanned: [],
+  totalmathdone : (this.targetBG * this.currentBG),//give me the total cost of the specific item
+  
+  
+  //         NEED HELP BELOW
+  totalOfAllSales : this.totalmathdone + this.totalmathdone, // not working
+
+  
+  
+  itemsScanned: function() {
+    this.todaysTimesingroup.push(this.todaysTime);
+    this.todaysDayingroup.push(this.todaysDay);
+     
+    console.log(`Time: ${this.todaysTime}`);
+    console.log(`Today is: ${this.todaysDay}`);
+    console.log(` Arons BG levels were ${itemScanner.targetBG}, on ${this.todaysDay} `)
+
+  },
+  
+  itemsNotScanned: function() {
+    if (this.todaysTime !== this.lowBGlevel)
+      console.log(`!!!WARNING ARON HAS HIGH BG!!!`);
+    },
+    
+  
+  }
+      
+function scanId(id, name, inout, amount, price) {
+  itemScanner.todaysTime = id;
+  itemScanner.todaysDay = name;
+  itemScanner.targetBG = amount;
+  itemScanner.currentBG = price;
+  totalCostOfItem = (((amount) - (price)) / 350);
+  
+  if (inout == "in") {
+    itemScanner.itemsScanned();
+   console.log(totalCostOfItem)
+    
+  } else if (inout !== "in") {
+    itemScanner.itemsNotScanned();
+  } 
+  console.log(` ${""} `);
+   
+}
+
+// will cauase it to be undefined
+
+// function total(){
+//  totalP= itemScanner.totalmathdone + itemScanner.totalmathdone;
+//  return totalP ;
+// }
+
+ 
+scanId("730", "Monday", "in", 150, 465);
+scanId("1000", "Tuesday", "in", 180,387);
+scanId("1300", "Wedneday", "in", 167,435);
+scanId("1600", "Thursday", "in", 5 ,2);
+scanId("1900", "Friday", "in", 4, 4);
+
+
+console.log (` <..::THERE IS NOTHING IN THE WORLD MOMMY AND DADDY WOULDNT DO FOR YOU!::..> `);
+console.log(` ${""} `); // adding a space
+// console.log(` That Cost a total cost  of your groceries is ${itemScanner.totalP} dollars`) 
+console.log(` ${""} `);
+
+
+//            NOT SCANNED YET
+scanId("25941", "Watermelon", "out", 1,3);
+scanId("84342", "Grapes", "out", 1,2);
+
+
