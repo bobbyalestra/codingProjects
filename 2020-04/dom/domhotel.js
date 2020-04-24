bd3b2c7b695d90e45d9127e39c82fc10d1e4c923
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-24 07:58:15 -0400

    uodate

--- 2020-04/dom/domhotel.js
+++ 2020-04/dom/domhotel.js
@@ -2,14 +2,22 @@ document.getElementById('slogan').innerText = 'Vitory';
 document.getElementById('hotelName').innerText = 'The DOM Hotel'
 
 
-
-
-
 document.getElementById('availRoomslists"').innerHTML = linesOfHtml;
-let availRoomslists = [101, 102, 103, 104]
 
-let linesOfHtml = "<select>"  
+
+let domHotel ={
+    
+
+
+
+  availableRooms : [101, 102, 103, 104],
+    roomType : ['singles', 'Doubles', 'Queen', 'King'],
+
+}
+
+  linesOfHtml = "<select>"  
 for (i = 0; i < availRoomslists.lenth; i++)
 linesOfHtml = linesOfHtml + `<option value = '${availRoomslists[i]}'>${availRoomslists[i]} </option>`
 
 linesOfHtml = linesOfHtml + "</select>"
+
