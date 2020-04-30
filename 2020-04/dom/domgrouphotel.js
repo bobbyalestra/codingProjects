1125bc87a1678d91e5cb4ae5a22b8b01b31eb4ff
Robert Alestra <bobbyalestra135@gmail.com>
2020-04-29 21:45:25 -0400

    unbook

--- 2020-04/dom/domgrouphotel.js
+++ 2020-04/dom/domgrouphotel.js
@@ -1,104 +1,77 @@
-document.getElementById("hotelName").innerText = "DOM Hotel";
-document.getElementById("slogan").innerText = "Where guests can CODE all night!";
+document.getElementById("hotelName").innerText = "DOM Hotel!";
+document.getElementById("slogan").innerText = "Where guests CODE all night!";
 
 let hotel = {
-    availableRooms: [ [101, 102, 103, 104], [201, 202, 203, 204], [301, 302, 303], [401, 402], ],
+    availableRooms: [[101, 102, 103, 104], [201, 202, 203, 204], [301, 302, 303], [401, 402]],
     bookedRooms: [],
-    unbookedRooms: [],
-    roomTypes: ['Single', 'Double', 'King', 'Queen'],
+    roomTypes: ["Single", "Double", "King", "Queen"],
     roomRates: [300, 350, 450, 550],
 
     completeBookedRoomsArrays: function() {
         for (let i = 0; i < this.availableRooms.length; i++) {
-            this.bookedRooms.push( [] );
+            this.bookedRooms.push( [] )
         }
-
     },
-
     bookSelectedRoom: function() {
-        let selectedRoom = document.getElementById('availableSelect').value;
-        for (let i = 0; i < this.availableRooms.length; i++ ) {
-            for (let j = 0; j < this.availableRooms[i].length; j++ ) {
-
-                if (selectedRoom == this.availableRooms[i][j]) {
-                    this.bookedRooms[i].push(this.availableRooms[i].splice(this.availableRooms[i].indexOf(selectedRoom),1));
+        let selectedAvailableRoom = parseInt(document.getElementById("availableSelect").value);
+        // console.log(selectedAvailableRoom);
+        for (let i = 0; i < this.availableRooms.length; i++) {
+            for (let j= 0; j < this.availableRooms[i].length; j++) {
+                if (selectedAvailableRoom == this.availableRooms[i][j]) {
+                    console.log(typeof(selectedAvailableRoom));
+                    console.log(typeof(this.availableRooms[i][j]));
+                    console.log(selectedAvailableRoom);
+                    console.log(this.availableRooms[i][j]);
+                    this.bookedRooms[i].push(this.availableRooms[i].splice(this.availableRooms[i].indexOf(selectedAvailableRoom),1))
+                    // this.bookedRooms[i].push(this.availableRooms[i][j]);
+                   
                 }
-                
-
-            }        
-        }
-
-        this.updateAvailableRooms();
-        this.updateBookedRooms();
-        
-
-    },
-// in progress
-    UnbookSelectedRoom: function() {
-        let selectedRoom = document.getElementById('availableSelect').value;
-        for (let i = 0; i < this.unbookedRooms.length; i++ ) {
-            // for (let j = 0; j < this.unbookedRooms[i].length; j++ ) {
-
-                // if (selectedRoom == this.unbookedRooms[i][j]) {
-                //     this.unbookedRooms[i].push(this.bookedRooms[i].splice(this.unbookedRooms[i].indexOf(selectedRoom),1));
-            //    }
-                if (selectedRoom == this.availableRooms[i][j]) {
-                    this.unbookedRooms[i].push(this.availableRooms);
-
             }
         }
-        this.UnbookSelectedRoom();
+        this.updateAvailableRooms();
+        this.updateBookedRooms();
     },
 
+    unbookSelectedRoom: function() {
+        let selectedUnavailableRoom = parseInt(document.getElementById("unavailableSelect").value);
+        console.log(selectedUnavailableRoom);
+        for (let i = 0; i < this.bookedRooms.length; i++) {
+            for (let j = 0; j < this.bookedRooms[i].length; j++) {
+                if (selectedUnavailableRoom == this.bookedRooms[i][j]) {
+                    console.log(typeof(selectedUnavailableRoom));
+                    this.bookedRooms[i].splice(this.bookedRooms[i].indexOf(this.bookedRooms[i][j]),1);
+                    this.availableRooms[i].push(this.bookedRooms[i][j]);
+                }
+            }
+        }
+        this.updateAvailableRooms();
+        this.updateBookedRooms();
+    },
 
     updateAvailableRooms: function() {
-        let dropdown = '<select id="availableSelect">';
-
-        for (let i = 0; i < this.availableRooms.length; i++ ) {
-            for (let j = 0; j < this.availableRooms[i].length; j++ ) {
-                dropdown = dropdown + `<option value = '${this.availableRooms[i][j]}'>${this.availableRooms[i][j]}</option>`;
-
-            }        
+        let dropdown = `<select id = "availableSelect">`
+        for (let i = 0; i < this.availableRooms.length; i++) {
+            for (let j= 0; j < this.availableRooms[i].length; j++) {
+                dropdown = dropdown + `<option value = "${this.availableRooms[i][j]}">${this.availableRooms[i][j]}</option>`
+            }
         }
-        dropdown = dropdown + '</select>';
-        document.getElementById('dropDownAvailable').innerHTML = dropdown;
-        
-    },
+                dropdown = dropdown + `</select>`
+                document.getElementById("dropDownAvailable").innerHTML = dropdown;
 
+    },
     updateBookedRooms: function() {
-        let dropdown = '<select>';
-
-        for (let i = 0; i < this.bookedRooms.length; i++ ) {
-            for (let j = 0; j < this.bookedRooms[i].length; j++ ) {
-                dropdown = dropdown + `<option value = '${this.bookedRooms[i][j]}'>${this.bookedRooms[i][j]}</option>`;
-
-            }        
+        let dropdown = `<select id = "unavailableSelect">`
+        for (let i = 0; i < this.bookedRooms.length; i++) {
+            for (let j= 0; j < this.bookedRooms[i].length; j++) {
+                dropdown = dropdown + `<option value = "${this.bookedRooms[i][j]}">${this.bookedRooms[i][j]}</option>`
+            }
         }
-        dropdown = dropdown + '</select>';
-        document.getElementById('dropDownBooked').innerHTML = dropdown;
-        
-    },
+                dropdown = dropdown + `</select>`
+                document.getElementById("dropDownBooked").innerHTML = dropdown;
 
-
-
-    updateUnbookedRooms: function() {
-        let dropdown = '<select>';
-
-        for (let i = 0; i < this.unbookedRooms.length; i++ ) {
-            
-                dropdown = dropdown + `<option value = '${this.unbookedRooms[i][j]}'>${this.unbookedRooms[i][j]}</option>`;
-
-                    
-        }
-
-        dropdown = dropdown + '</select>';
-        document.getElementById('dropDownUnbooked').innerHTML = dropdown;
-        
-    },
-
+    }
 }
-
 hotel.updateAvailableRooms();
 hotel.updateBookedRooms();
 hotel.completeBookedRoomsArrays();
-hotel.updateUnbookedRooms();
+console.log(document.getElementById("dropDownAvailable").innerText)
