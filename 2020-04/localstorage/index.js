471c98bdf75ddac1864536fdcf347ae4dd601697
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-01 22:58:01 -0400

    updaytes

---
+++ 2020-04/localstorage/index.js
@@ -0,0 +1,14 @@
+
+
+function updateName (){
+    localStorage.setItem('name1', document.getElementById('fName').value);
+    document.getElementById("firstName").innerText = localStorage.getItem('name1');
+
+    localStorage.setItem('name2', document.getElementById('lName').value);
+    document.getElementById("lastName").innerText = localStorage.getItem('name2');
+
+    
+}
+document.getElementById("firstName").innerText = localStorage.getItem('name1');
+
+document.getElementById("lastName").innerText = localStorage.getItem('name2');
---
+++ 2020-04/localstorage/listofstudents.js
@@ -0,0 +1,26 @@
+let newStudentNames = [];
+
+
+let oldArrayNames = JSON.parse(localStorage.getItem('sNames'));
+console.log(oldArrayNames);
+
+
+function addStudentName() {
+    newStudentNames.push(document.getElementById('name').value);
+
+    let studentNames = oldArrayNames.concat(newStudentNames);
+    document.getElementById('name').value = "";
+
+let JSONnames = JSON.stringify(studentNames)
+
+ localStorage.setItem('sNames',  JSONnames);
+
+}
+let list ='<ul>' ;
+    for (i = 0; i <oldArrayNames.length; i++){
+    list = list + `<li>${oldArrayNames[i]}</li>`;
+    }
+    list = list + '</ul>'
+
+document.getElementById('listOfNames').innerText = list;
+
