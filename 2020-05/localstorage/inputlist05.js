fe47eead84cd2f6a5a94a87907fb02f2236f90f6
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-05 10:17:45 -0400

    update the list on html

--- 2020-05/localstorage/inputlist05.js
+++ 2020-05/localstorage/inputlist05.js
@@ -1,6 +1,12 @@
 
 let arrayNames = [];
 let parsedArray = [];
+let oldArrayNames =[];
+
+let oldArrayNames=JSON.parse(localStorage.getItem('arrayNames'));
+
+
+
 function addnameToArray(){
    let nameToAdd= document.getElementById('inputBox').value
    if (nameToAdd.length != 0){
@@ -17,9 +23,9 @@ function addnameToArray(){
     parsedArray = JSON.parse(localStorage.getItem('lsArrayNames'));
    }
 
-   for (i =0; i < document.getElementById('theList').length; i++ )
+   for (i =0; i < oldArrayNames.length; i++ )
    let htmlText = '<ul>'
-   
+
 }
 
 
