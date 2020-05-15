63dd5f623e6bc4d32adffd7fe2a9b2fac6cc82d8
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-15 11:04:33 -0400

    updates on black jack app

--- 2020-05/blackjack1.js
+++ 2020-05/blackjack1.js
@@ -4,8 +4,8 @@ let cards = {
     player1Cards: [],
     player1CardsTotal: 0,
     dealerCards: [],
-    dealerCardsTotal: 0
-    
+    dealerCardsTotal: 0,
+    busted: 0,
   
   }
   
@@ -20,46 +20,11 @@ let cards = {
       
       // Dont forget to add the update player cards method here
       
-  
-      totalPlayer1Cards: function () {
-        for (let i = 0; i < cards.player1Cards.length; i++) {
-          cards.player1CardsTotal += cards.player1Cards[i];
-  
-        }
-  
-        document.getElementById('player1CardsTotal').innerText = cards.player1CardsTotal;
-        cards.player1CardsTotal = 0;
-  
-      },
-  
-  
-      //dealer gets their cards
-  
-      initialDealerCards: function() {
-        for (let i = 0; i < 2; i++) {
-          cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
-        }
-        // player 1 cards updated 
-        this.updatePlayer1Cards();
-  
-    },
-  
-  
-      totalDealerCards: function() {
-        for (let i = 0; i < cards.dealerCards.length; i++) {
-          cards.dealerCardsTotal += cards.dealerCards[i];
-        }
-        document.getElementById('dealerCardsTotal').innerText = cards.dealerCardsTotal;
-  
-      },
-  
-      //Hit and stand buttons clicked from player 1
-  
-      hitMe: function() {
-        for (let i = 0; i < 1; i++) {
+      player1Hit: function() {
+        if( cards.player1Cards.length === 0){
+
+        }else{
           cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
-      
-  
         }
         this.updatePlayer1Cards();
   
@@ -74,8 +39,87 @@ let cards = {
         html += `</ul>`;
         document.getElementById('player1Cards').innerHTML = html;
         this.totalPlayer1Cards();
+        
+      },
+     
+
+
+      totalPlayer1Cards: function () {
+        for (let i = 0; i < cards.player1Cards.length; i++) {
+          cards.player1CardsTotal += cards.player1Cards[i];
   
-      }
-      };
-    //call functions
-    cardMethod.initialDeal();
+        }
+  
+        document.getElementById('player1CardsTotal').innerText = cards.player1CardsTotal;
+        if (cards.player1CardsTotal > 21) {
+            cards.busted = 1
+            setTimeout(function() {alert('BUSTED! Give us your money!!')} , 100)
+            
+            cards.player1Cards= [];
+        }else if (cards.player1CardsTotal = 21){
+            // chill for a bit
+        }
+        cards.player1CardsTotal = 0;
+        
+      },
+      newGame: function (){
+        if( cards.player1Cards.length === 0){
+        this.initialDeal(); 
+        }
+    },
+    
+    player1Stand: function (){
+        this.dealerPlayLogic();
+        },
+  
+      //dealer gets their cards
+  
+      dealerPlayLogic: function (){
+        setTimeout(100)
+        
+        if (player1CardsTotal == 21){
+              while (dealerCardsTotal <= 21) {
+                cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+              }
+          }
+        
+      },
+
+
+      initialDealerCards: function() {
+         
+        
+          cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+        
+        // player 1 cards updated 
+        this.updateDealerCards();
+  
+    },
+  
+    // setTimeout(function() {alert('BUSTED! Give us your money!!')} , 100)
+  
+      totalDealerCards: function() {
+          
+        for (let i = 0; i < cards.dealerCards.length; i++) {
+          cards.dealerCardsTotal += cards.dealerCards[i];
+        }
+    
+        document.getElementById('dealerCardsTotal').innerText = cards.dealerCardsTotal;
+  
+    },
+    updateDealerCards: function (){
+        let html = `<ul>`;
+        for (let i = 0; i < cards.dealerCards.length; i++) {
+          html += `<li>${cards.dealerCards[i]}</li>`
+  
+        }
+        html += `</ul>`;
+        document.getElementById('dealerCards').innerHTML = html;
+        this.totalDealerCards();
+        
+    }
+}
+    cardMethod.totalDealerCards()
+      cardMethod.initialDeal()
+
+      
